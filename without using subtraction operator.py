0570eef13e240e4a07d0b99b079e3467918f14eb
sachinprajapati2314 <sachinprajapati2314@gmail.com>
2023-10-06 14:57:36 +0530

    without (-) operator

---
+++ without using subtraction operator.py
@@ -0,0 +1,9 @@
+num_1 = int(input("enter the first number = "))
+num_2 = int(input("enter the second number = "))
+
+while (num_2 != 0):
+    temp = ~num_1 & num_2
+    num_1 = num_1 ^ num_2
+    num_2 = temp << 1
+
+print(num_1)
