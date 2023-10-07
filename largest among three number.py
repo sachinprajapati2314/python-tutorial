d137ab3acca0212762fdb44d3823d3a2b9f89a21
sachinprajapati2314 <sachinprajapati2314@gmail.com>
2023-10-07 20:47:44 +0530

    largest number

---
+++ largest among three number.py
@@ -0,0 +1,15 @@
+#  largestfind among three numbers using conditional operator 
+
+a = int(input("enter the first number = "))
+b = int(input("enter the second number = "))
+c = int(input("enter the third number = "))
+
+if (a >= b) and (a >= c):
+    print(a, "is the largest number ")
+elif(b >= a) and (b >= c):
+     print(b, "is the largest number ")
+#elif(c >= a) and (c >= b):
+#      print(c, "is the largest number ")
+else:
+     print(c, "is the largest number ")
+    
