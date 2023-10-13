1e30e22c605e9e33e0a571f6f0bc6942ac6a5bc0
sachinprajapati2314 <sachinprajapati2314@gmail.com>
2023-10-13 22:29:23 +0530

    fibonacci series

--- fibonacci series.py
+++ fibonacci series.py
@@ -1,13 +1,16 @@
+# number input
 num = int(input("enter the number = "))
 
 n1 = 0
 n2 = 1
 
+# 0 and 1 print
 print(n1)
 print(n2)
+
+# for loop 
 for i in range(1, num-1):
     n3 = n1 + n2
-    
     print(n3)
     n1 = n2
     n2 = n3
