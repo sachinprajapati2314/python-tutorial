01bc3715c6fb6f7b7cc6e85c91f60d305bc43008
sachinprajapati2314 <sachinprajapati2314@gmail.com>
2023-10-20 20:22:51 +0530

    swapping of string

---
+++ swapping of two string.py
@@ -0,0 +1,9 @@
+a = input("enter the first name = ")
+b = input("enter the second name = ")
+
+if a :
+    a,b = b,a
+    print("after swapping first name = ",a)
+    print("after swapping second name = ",b)
+else:
+    None
