f6300bc83581b6d5f547367c2846ea0de15a0f1f
sachinprajapati2314 <sachinprajapati2314@gmail.com>
2023-10-20 12:59:13 +0530

    swap without third number

---
+++ swap number without using third variable.py
@@ -0,0 +1,9 @@
+a = int(input("enter first number = "))
+b = int(input("enter second number = "))
+
+a = a - b
+b = a + b
+a = b - a
+
+print('after swaping first number = ',a)
+print('after swaping second number = ',b)
