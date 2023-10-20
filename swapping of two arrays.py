c14e11069ed3ed1e3a8d3921aebf669a06e55b61
sachinprajapati2314 <sachinprajapati2314@gmail.com>
2023-10-20 16:53:58 +0530

    swapping of arrays

---
+++ swapping of two arrays.py
@@ -0,0 +1,30 @@
+a = [1,2,3,4]
+b = [5,6,7,8]
+
+print("before swapping = ")
+print(a)
+print(b)
+
+t = a
+a = b
+b = t
+
+print("after swapping = ")
+print(a)
+print(b)
+
+
+m = ["hii","how","hello"]
+print("before swapping = ",m)
+
+g = m[0]
+h = m[1]
+j = m[2]
+
+t = g
+g = h
+h = j
+j = t
+
+p = [g,h,j]
+print("after swapping = ",p)
