23b58995d92cb2f85a57a7f748bf48c023107a6b
sachinprajapati2314 <sachinprajapati2314@gmail.com>
2023-10-20 12:38:35 +0530

    swap number

---
+++ swap two number.py
@@ -0,0 +1,9 @@
+a = int(input("enter first number = "))
+b = int(input("enter second number = "))
+
+t = a
+a = b
+b = t
+
+print("after swaping first number = ",a)
+print("after swaping second number = ",b)
