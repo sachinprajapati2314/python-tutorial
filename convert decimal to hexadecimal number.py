11f6247803ef0b864d34332589a242c5de114215
sachinprajapati2314 <sachinprajapati2314@gmail.com>
2023-10-20 22:48:44 +0530

    decimal to hexadecimal

--- convert decimal to hexadecimal number.py
+++ convert decimal to hexadecimal number.py
@@ -1,7 +1,12 @@
-
 a = int(input("enter decimal number = "))
 
-d =hex(a)
+# convert hexadecimal
+d = hex(a)
+
+# string position
 f = str(d[2:])
+
+# convert upper case
 b = f.upper()
+
 print("hexadecimal number = ",(b))
