fd58f233f19daa9f37f669fe3609a616f430fd21
sachinprajapati2314 <sachinprajapati2314@gmail.com>
2023-10-20 17:09:59 +0530

    swapping of arrays

--- swapping of two arrays.py
+++ swapping of two arrays.py
@@ -1,3 +1,4 @@
+# 1 method
 a = [1,2,3,4]
 b = [5,6,7,8]
 
@@ -13,7 +14,7 @@ print("after swapping = ")
 print(a)
 print(b)
 
-
+# 2 method
 m = ["hii","how","hello"]
 print("before swapping = ",m)
 
