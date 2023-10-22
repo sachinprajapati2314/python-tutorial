fdf671a6a1b2fcb75964e57b6c7b4b0399f04881
sachinprajapati2314 <sachinprajapati2314@gmail.com>
2023-10-22 15:45:02 +0530

    upper to lower & lower to upper

---
+++ convert string case.py
@@ -0,0 +1,9 @@
+# convert upper to lower case
+a = input("enter upper str = ")
+u_c = a.lower()
+print("lower case = ",u_c)
+
+# convert lower to upper
+b = input("\nenter lower str = ")
+l_c = b.upper()
+print("upper case = ",l_c)
