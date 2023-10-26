ac22744cf3f873ae7f8dfe271e295d56454826a5
sachinprajapati2314 <sachinprajapati2314@gmail.com>
2023-10-26 21:13:27 +0530

    consonants from string

--- delete the all consonants from string.py
+++ delete the all consonants from string.py
@@ -1,4 +1,3 @@
-# in the first way
 a = str( input("enter string = ") )
 
 s = []
@@ -9,12 +8,3 @@ for i in a:
 s = ' '.join(s)
 
 print("consonants = ",str(s))
-
-# in the second way
-st = input("enter string = ")
-k = str("")
-
-for j in st:
-  if j in "aeiouAEIOU":
-     l=k.removeprefix()
-print(str(l))
