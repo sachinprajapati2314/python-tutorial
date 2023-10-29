f995f1842be1373bdca8ef2a6b253fced2db47a2
sachinprajapati2314 <sachinprajapati2314@gmail.com>
2023-10-29 16:03:22 +0530

    copy file 1 to file 2

--- copy a file from one location to other location.py
+++ copy a file from one location to other location.py
@@ -1,13 +1,18 @@
 import shutil
+
+#file no.1
 f = open("my txt",'w')
 f.write("hii sir How are you\n")
 f.write("hii sachin how can help you ?")
 f.close()
 
+# file no.2
 f1 = open("myfile txt","w")
 f1.close
 
+# copy file no.1 to file no.2
 shutil.copyfile("my txt","myfile txt")
 
+# read file no.2
 f1 = open("myfile txt","r")
 print(f1.read())
