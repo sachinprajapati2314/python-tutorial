85f9b404bd6546b97798e4d297b7f8fff83bf951
sachinprajapati2314 <sachinprajapati2314@gmail.com>
2023-11-04 18:17:48 +0530

    largest element og arr

---
+++ find out largest element of an array .py
@@ -0,0 +1,9 @@
+a = int(input("enter the number of elements (1 to 10) = "))
+l = []
+
+for i in range(1,a+1):
+    a = input(f"enter number {i} = ")
+    l.append(a)
+
+l.sort()
+print("largest element of arr =",l[-1])
