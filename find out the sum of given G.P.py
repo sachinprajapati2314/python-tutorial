6ea13379ac516288e99af746422e046e33581320
sachinprajapati2314 <sachinprajapati2314@gmail.com>
2023-11-04 11:28:05 +0530

    sum of given G.P

---
+++ find out the sum of given G.P.py
@@ -0,0 +1,18 @@
+a = int(input("enter the first number of G.P series = "))
+n = int(input("enter the total number in G.P series = "))
+r = int(input("enter the common ratio of G.P series = "))
+
+sum = (a * (1 - pow(r,n+1))) // (1-r)
+tn = a * (pow(r,n-1))
+
+t1 = a
+t2 = a
+
+for i in range(n):
+    tr = r * t1
+    print("{}".format(tr))
+    t1 = tr
+    t2 = t2 * tr
+
+#print("term of G.p =",tn)
+print("sum of given G.P =",sum)
