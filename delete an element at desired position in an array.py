56697c3facbac1e127ef97b87001517a362430fe
sachinprajapati2314 <sachinprajapati2314@gmail.com>
2023-11-04 22:31:43 +0530

    delete element desired positionin arr

---
+++ delete an element at desired position in an array.py
@@ -0,0 +1,13 @@
+a = int(input("enter the number of elements (1 to 10) = "))
+l = []
+
+print("enter only (11 - 99)")
+for i in range(1,a+1):
+    a = input(f"enter number {i} = ")
+    l.append(a)
+
+print("arr =",l)
+r = int(input("enter delete desired position(start- 0) = "))
+
+l.remove(l[r])
+print("after delete desired position in arr =",l)
