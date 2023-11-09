dc5fe07b9874ae51e9a07f9405714be94dceb0ec
sachinprajapati2314 <sachinprajapati2314@gmail.com>
2023-11-09 15:34:29 +0530

    area of rectangle

---
+++ find the area of rectangle.py
@@ -0,0 +1,6 @@
+base = int(input("enter the base of the rectangle  = "))
+height = int(input("enter the height of the rectangle = "))
+
+area = base * height
+
+print("the area of rectangle = ",area)
