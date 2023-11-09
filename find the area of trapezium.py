896fc8f1e1deb0723837520d155b5b19edd15839
sachinprajapati2314 <sachinprajapati2314@gmail.com>
2023-11-09 15:59:46 +0530

    area of trapezium

---
+++ find the area of trapezium.py
@@ -0,0 +1,7 @@
+a = int(input("enter the base a of trapezium = "))
+b = int(input("enter the base b of trapezium = "))
+h = int(input("enter the height of trapezium = "))
+
+area = ((a + b) * h) / 2
+
+print("the area of trapezium = ",area)
